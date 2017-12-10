dd34d13ef3c2c969de4009435ee9152c04533c7f
Michael Okarimia <git@michaelokarimia.com>
2017-12-10 11:45:47 +0000

    Adds public API key & empty private key file
    Adds the public key to source control but not private key

--- client.py
+++ client.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 
+
+apiPublicKey = '3ed7f03032576c0b754b5b356266e05b'
+
+apiSecretPath = './privateKey.txt'
+
 class Client:
 
     def getAllCharacters(self):
-        print("All Characters")
+
+        secretFile = open(apiSecretPath, 'r')
+
+        secret = secretFile.readline()
+
+        print("Public key: " + apiPublicKey)
+        print("Private key: " + secret)
 
 
 Client.getAllCharacters(Client)
